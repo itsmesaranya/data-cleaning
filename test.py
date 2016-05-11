c2579524fc8b4eb84f974c8c444f7190f96042a9
Saranya <itsmesaranya@users.noreply.github.com>
2016-05-11 21:53:52 +0530

    Add data cleaning test files

---
+++ test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 08 12:39:02 2016
+
+@author: tom
+"""
+
+import re
+import pandas as pd
+
+df = pd.read_csv('data_source_test.csv')
+
+count = 0
+for row_index, row in df.iterrows():
+    if re.search(r"\\r\\n", str(row)):
+        print type(row)
+        #re.sub(r'\\r','',str(row))
+        row.replace({r'\\r\\n': ''} , regex=True)        
+        print row
+        count += 1
+        
+print count
+    
